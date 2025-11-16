aacdbcbcf2466e8880b52da096145a60c6762128
Lara Geschonneck <larageschonneck@Mac.fritz.box>
2025-11-16 13:01:27 +0100

    h1 bearbeitet

---
+++ src/h1/H1_main.java
@@ -0,0 +1,18 @@
+package h1;
+
+import java.util.Arrays;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+		int[] myArray = {675, 99999, 1, 8003};
+		
+		System.out.print("Umgedrehtes Array: ");
+		
+	for (int i = myArray.length - 1; i >= 0; i--) {
+		System.out.print(myArray[i] + " ");
+	}
+
+	}
+
+}

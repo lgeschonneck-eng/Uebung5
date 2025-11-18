bd2accbb2bbd61116016300d6e5acb5478062498
Lara Geschonneck <larageschonneck@MacBook-Pro-von-Lara.local>
2025-11-18 10:36:49 +0100

    p1 bearbeitet

---
+++ src/p1/P1_main.java
@@ -0,0 +1,18 @@
+package p1;
+
+public class P1_main {
+
+	public static void main(String[] args) {
+	
+		for ( int x = 0; x <= 11; x++) {
+		System.out.println(x);
+		}
+
+
+int x = 0;
+do {
+System.out.println(x);
+x++;
+} while (x <= 11);
+	}
+}
